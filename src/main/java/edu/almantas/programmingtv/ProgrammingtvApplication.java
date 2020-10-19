468434004f81258ad582c7e5ddc18ce027f63722
Almantas Karpavičius <almantusk@gmail.com>
2020-10-19 22:52:35 +0300

    Added base Spring Boot Web project

---
+++ src/main/java/edu/almantas/programmingtv/ProgrammingtvApplication.java
@@ -0,0 +1,13 @@
+package edu.almantas.programmingtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/almantas/programmingtv/ProgrammingtvApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.almantas.programmingtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
