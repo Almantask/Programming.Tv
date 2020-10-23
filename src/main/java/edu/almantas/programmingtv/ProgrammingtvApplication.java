70d611e2ff36db189a9cb10daa6dcb9dd9d92fa5
Almantas Karpavičius <almantusk@gmail.com>
2020-10-23 22:17:38 +0300

    Re-added embedded tomcat host

--- src/main/java/edu/almantas/programmingtv/ProgrammingtvApplication.java
+++ src/main/java/edu/almantas/programmingtv/ProgrammingtvApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgrammingtvApplication {
+public class ProgrammingtvApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgrammingtvApplication.class, args);
